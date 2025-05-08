6dd0c0bb755daf7df602f69f4a12baedf788251f
santimulet12 <santiagomulet2008@gmail.com>
2025-05-07 21:53:45 -0300

    Configuración inicial

---
+++ numerosFelicesEjercicio/src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+package org.example
+
+//Enunciado:
+//
+//Un número feliz es un número que eventualmente llega a 1 si reemplazás el número por la suma de los cuadrados de sus dígitos repetidamente. Si entra en un ciclo que no incluye el 1, entonces no es un número feliz.
+//
+//Ejemplos:
+//
+//    19 es un número feliz:
+//    12+92=8212+92=82
+//    82+22=6882+22=68
+//    62+82=10062+82=100
+//    12+02+02=112+02+02=1
+
+fun main(){
+
+}
